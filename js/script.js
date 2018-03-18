2b52c741469d52ca253b5790ee8ac5d13c956342
Deepak Bidyabhusan Das <das.bidyabhusan@gmail.com>
2018-03-18 11:36:15 +0530

    custom animation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 $(function() {
-  $(".blue-box").slideUp(1000);
-  $(".blue-box").slideDown(1000);
-  //$(".blue-box").slideToggle(1000);
-  // $("p").hide();
-  // $("p").slideDown(2000);
+  $(".blue-box").animate({
+    "margin-left":"+=200px"
+  },1000);
 });
