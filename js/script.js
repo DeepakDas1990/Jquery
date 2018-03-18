1c300ab691d7e9abae52aa311d5304a05e6721c9
Deepak Bidyabhusan Das <das.bidyabhusan@gmail.com>
2018-03-18 11:18:47 +0530

    slideup,slidedown,slidetoggle method

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(function() {
-  $(".blue-box").hide();
-  //$(".blue-box").show();
-  //$(".blue-box").toggle();
+  $(".blue-box").slideUp(1000);
+  $(".blue-box").slideDown(1000);
+  //$(".blue-box").slideToggle(1000);
+  // $("p").hide();
+  // $("p").slideDown(2000);
 });
