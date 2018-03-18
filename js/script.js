1c6b973ef589ffd9f04e67d17697fb22135e1fa7
Deepak Bidyabhusan Das <das.bidyabhusan@gmail.com>
2018-03-18 09:30:26 +0530

    fadeout

--- js/script.js
+++ js/script.js
@@ -2,5 +2,8 @@ $(function() {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-  // $(".red-box").fadeOut(2000);
+  $(".red-box").fadeOut(2000);
+  //$(".red-box").fadeOut();
+  //$(".red-box").fadeOut("slow");
+  //$(".red-box").fadeOut("fast");
 });
