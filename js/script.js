720a1a55304301d46df3818af487b8ce4b3efee9
Deepak Bidyabhusan Das <das.bidyabhusan@gmail.com>
2018-03-19 22:44:27 +0530

    another custom animation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 $(function () {
   $(".blue-box").animate({
     marginLeft: "+=200px",
+    fontSize: "0px",
     opacity: "0",
     height: "25px",
     width: "25px",
     marginTop: "25px"
   }, 1000);
+
+  $("p").animate({
+    fontSize: "20px"
+  },1000);
 });
