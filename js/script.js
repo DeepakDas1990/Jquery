c8befc394880df9efd9a04d74fc832841319eb16
Deepak Bidyabhusan Das <das.bidyabhusan@gmail.com>
2018-03-20 22:14:28 +0530

    Delay Method and Chaining

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
 $(function () {
-  $(".blue-box").animate({
-    marginLeft: "+=200px",
-    fontSize: "0px",
-    opacity: "0",
-    height: "25px",
-    width: "25px",
-    marginTop: "25px"
-  }, 1000);
-
-  $("p").animate({
-    fontSize: "20px"
-  },1000);
+  $(".red-box").fadeTo(1000,0.2);
+  $(".green-box").delay(1000).fadeTo(1000,0.5);
+  $(".blue-box").delay(2000).fadeTo(1000,0.8).fadeOut().delay(1000).fadeIn();
 });
