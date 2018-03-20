040968e8cfb24e560e85413e0e9f605a64ed47f8
Deepak Bidyabhusan Das <das.bidyabhusan@gmail.com>
2018-03-20 22:33:46 +0530

    Call back functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 $(function () {
-  $(".red-box").fadeTo(1000,0.2);
-  $(".green-box").delay(1000).fadeTo(1000,0.5);
-  $(".blue-box").delay(2000).fadeTo(1000,0.8).fadeOut().delay(1000).fadeIn();
+  $(".red-box").fadeTo(1000,0,function(){
+    $(".green-box").fadeTo(1000,0,function(){
+      $(".blue-box").fadeTo(1000,0,function () {
+        $(".blue-box").fadeTo(1000,1,function () {
+          $(".green-box").fadeTo(1000,1,function () {
+            $(".red-box").fadeTo(1000,1);
+          });
+        });
+      });
+    });
+  });
+  // $(".red-box").fadeTo(1000,0,function(){
+  //   $(".green-box").fadeTo(1000,0,function(){
+  //     $(".blue-box").fadeTo(1000,0,function () {
+  //       alert("Animation Finished");
+  //     });
+  //   });
+  // });
 });
+
+/* 
+call back funtions are executed only after parent function is executed.
+but in case of delay(), user can change delay time according to it's choice.
+*/
