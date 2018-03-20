fbf5d8e44a57b7430cfcfd34850e7458141cf566
Deepak Bidyabhusan Das <das.bidyabhusan@gmail.com>
2018-03-20 22:47:00 +0530

    A sample Sign In Window

--- js/script.js
+++ js/script.js
@@ -1,25 +1,3 @@
 $(function () {
-  $(".red-box").fadeTo(1000,0,function(){
-    $(".green-box").fadeTo(1000,0,function(){
-      $(".blue-box").fadeTo(1000,0,function () {
-        $(".blue-box").fadeTo(1000,1,function () {
-          $(".green-box").fadeTo(1000,1,function () {
-            $(".red-box").fadeTo(1000,1);
-          });
-        });
-      });
-    });
-  });
-  // $(".red-box").fadeTo(1000,0,function(){
-  //   $(".green-box").fadeTo(1000,0,function(){
-  //     $(".blue-box").fadeTo(1000,0,function () {
-  //       alert("Animation Finished");
-  //     });
-  //   });
-  // });
+  $(".lightbox").delay(500).fadeIn(1000).delay(500).fadeOut(1000);
 });
-
-/* 
-call back funtions are executed only after parent function is executed.
-but in case of delay(), user can change delay time according to it's choice.
-*/
