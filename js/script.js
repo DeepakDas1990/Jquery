26386e7914b751db0baa2d778ce024c2e7c61b11
Deepak Bidyabhusan Das <das.bidyabhusan@gmail.com>
2018-03-24 20:18:41 +0530

    css selectors

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(function () {
-  $(".lightbox").delay(500).fadeIn(1000).delay(500).fadeOut(1000);
+  //$("p").css("background-color","rgba(180,180,30,0.8)");
+  //$("p").css("background-color","yellow");
+  //$(".red-box").css("background-color","purple");
+  //$("#list").css("background-color","green");
+  //$("input[type='text']").css("background-color","yellow");
+  //$("h2,p,input").css("background-color","yellow");
+  //$("li:first").css("background-color","yellow");
+  //$("li:last").css("background-color","yellow");
+  //$("li:even").css("background-color","yellow");
+  //$("li:odd").css("background-color","rgba(180,180,30,0.8)");
+  $("input[type='email']").css("background-color","rgba(180,180,30,0.8)");
 });
