d6eb8138f3698e62d1b5ef3ec43ae5359a2e19d7
Deepak Bidyabhusan Das <das.bidyabhusan@gmail.com>
2018-03-26 22:36:41 +0530

    Jquery Methods for Filtering

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 $(function () {
-  $(":header").next().css("background-color","rgba(180,180,0,0.8)");
-  $("form").find("input").css("background-color","rgba(180,180,0,0.8)");
+  //$("#list").find("li").filter(":even").css("background-color","rgba(180,180,30,0.8)");
+  //$("#list").children("li").filter(":even").css("background-color","rgba(180,180,30,0.8)");
+  // $("li").filter(function (index) {
+  //   return (index % 2 == 1);
+  // }).css("background-color", "rgba(180,180,30,0.8)");
+  //$("li").first().css("background-color", "rgba(180,180,30,0.8)");
+  //$("li").last().css("background-color", "rgba(180,180,30,0.8)");
+  //$("li").eq(7).css("background-color", "rgba(180,180,30,0.8)");
+  //$("li").eq(-3).css("background-color", "rgba(180,180,30,0.8)");
+  //$("li").not(":first").css("background-color", "rgba(180,180,30,0.8)");
+  // $("li").not(function (index) {
+  //   return index % 2 == 0;
+  // }).css("background-color", "rgba(180,180,30,0.8)");
+  $("li").not("#list ul li").css("background-color", "rgba(180,180,30,0.8)");
 });
