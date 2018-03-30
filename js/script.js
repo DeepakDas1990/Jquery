aa57ac57b815f4f6e809373eb3ee82943945f6c7
unknown <deepak.b@Deepak.sgp.in>
2018-03-30 19:19:55 +0530

    Replace Elements in DOM

--- js/script.js
+++ js/script.js
@@ -1,25 +1,15 @@
 $(function () {
-  //$("ul ul:first").append("<li>This is the last sub-item</li>");
-  //$("ul ul:last").append("<li>This is the last sub-item</li>");
-  //$("ul ul:even").append("<li>This is the last sub-item</li>");
-  //$("ul ul:odd").append("<li>This is the last sub-item</li>");
-  //$("ul ul").append("<li>This is the last sub-item</li>");
-  //$("ul").append("<li>This is the last sub-item</li>");
+    //$("li").replaceWith("<li>Replaced.</li>"); 
+    //  $("li").replaceWith(function () {
+    //      return "<li>Replaced With Function</li>";
+    //  }); 
+    //var firstListItem = $("li:first");
+    //$("p:first").replaceWith(firstListItem);
+    //$("p").replaceWith(firstListItem);
 
-  //$("ul ul:first").prepend("<li>This is the last sub-item</li>");
-  //$("#content").prepend("<h4>Deepak Bidyabhusan Das</h4>");
-
-  //$(".red-box").after("<div class='red-box'>Another Red Box</div>");
-  //$(".blue-box").before("<div class='blue-box'>Another Red Box</div>");
-
-  // $(".blue-box").before(function () {
-  //   return "<div class='blue-box'>Another Blue Box</div>";
-  // });
-
-  //$(".blue-box").before($(".red-box"));
-  //$("p").after($("#list"));
-
-  $(".red-box").before("<div class='red-box'>Another Red Box</div>");
-  $(".blue-box").before("<div class='blue-box'>Another Blue Box</div>");
-  $(".green-box").before("<div class='green-box'>Another Green Box</div>");
+    var greenbox = $(".green-box");
+    // $(".red-box").replaceWith(greenbox);
+    // $(".blue-box").replaceWith(greenbox);
+    //$(".red-box,.blue-box").replaceWith(greenbox);
+    $(".red-box,.blue-box").replaceWith("<div class='green-box'>More Green</div>");
 });
