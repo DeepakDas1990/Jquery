584089fb4039081ca37b32bc5f1c8aa6a43cb358
unknown <deepak.b@Deepak.sgp.in>
2018-03-31 12:39:16 +0530

    Adding Hover Handlers

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
 $(function () {
-   $("#btn-click").click(function (event) {
-       console.log(event);
-       alert('Button was Clicked');
-   });
-   $(".red-box").click(function () {
-       alert("Red box is clicked");
-       $(this).fadeTo(1000,0.5);
-   });
-   $(".red-box").click();
-   /**
-    * $(".red-box").click();
-    * by writing this code, it will automatically invoke the click handler, while page load.
-    */
+    //    $("#btn-hover").hover(function () {
+    //        alert("button is hovered");
+    //    });
+
+    $(".green-box").hover(function () {
+        $(this).text("I was Hovered");
+    });
+
+    /**
+     * while hovering, hover event is executed twice, while enter and while leaving the elment.
+     */
 });
