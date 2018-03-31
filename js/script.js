61d0effc96de2e13e35a5ca9c6a14fa59adef434
unknown <deepak.b@Deepak.sgp.in>
2018-03-31 14:50:39 +0530

    Adding mouseenter and mouseleave handler

--- js/script.js
+++ js/script.js
@@ -1,13 +1,37 @@
 $(function () {
-    //    $("#btn-hover").hover(function () {
-    //        alert("button is hovered");
-    //    });
+    $("#btn-hover").hover(function () {
+        alert("button is hovered");
+    });
 
     $(".green-box").hover(function () {
         $(this).text("I was Hovered");
     });
 
-    /**
-     * while hovering, hover event is executed twice, while enter and while leaving the elment.
-     */
+    var bluebox = $(".blue-box");
+
+    // bluebox.mouseenter(function () {
+    //     $(this).fadeTo(500, 0.8);
+    // });
+
+    // bluebox.mouseleave(function () {
+    //     $(this).fadeTo(500, 1);
+    // });
+
+    // bluebox.mouseenter(function () {
+    //     $(this).stop().fadeTo(500, 0.8);
+    // });
+
+    // bluebox.mouseleave(function () {
+    //     $(this).stop().fadeTo(500, 1);
+    // });
+
+
+    //hover(mouseenter,mouseleave)
+    bluebox.hover(function() {
+        $(this).stop().fadeTo(500,0.7);
+    },function() {
+        $(this).stop().fadeTo(500,1);
+    });
+
+    // better practice to use stop() method
 });
