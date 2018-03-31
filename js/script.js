9b79ba12b113e9d25aee70732a6df8359a4941e3
unknown <deepak.b@Deepak.sgp.in>
2018-03-31 11:06:59 +0530

    changing the CSS properties of Elements

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 $(function () {
-    var galleryImage = $(".gallery").find("img").first();
-    var images = [
-        "images/laptop-mobile_small.jpg",
-        "images/laptop-on-table_small.jpg",
-        "images/people-office-group-team_small.jpg",
-    ];
+    $(".gallery").css("display","none"); 
 
-    var i = 0;
-    setInterval(function () {
-        i = (i + 1) % images.length;
-        galleryImage.fadeOut(function () {
-            $(this).attr("src",images[i]);
-            $(this).fadeIn();
-        });
-        console.log(galleryImage.attr("src"));
-    }, 3000);
+    var redbox = $(".red-box");
+    console.log(redbox.css("width"));
+    console.log(redbox.width());
+
+    redbox.css("background-color","#AA7700");
+    $("p").css("font-size","18px");
+
+    redbox.css("width","+=20px");   
+
+    var properties = $("p").css(["font-size","line-height","color"]);
+    console.log(properties);
+    console.log(properties["font-size"]);
+
+    redbox.css("user-select","none");
+
+    redbox.css("user-select",function () {
+        return "none";
+    })
 });
