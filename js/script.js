8362248dd23700e36fa369c9e59c0b96ce84ab41
unknown <deepak.b@Deepak.sgp.in>
2018-04-04 20:13:46 +0530

    Focus and Blur Events

--- js/script.js
+++ js/script.js
@@ -1,18 +1,35 @@
 $(document).ready(function () {
-    $("html").keydown(function (event) {
-        console.log(event.which);
-    });
+    // var inputFields = $("input[type='text'], input[type='email'], textarea");
+    // inputFields.focus(function(){
+    //     $(this).css("box-shadow","0 0 8px #666");
+    // });
+    // inputFields.blur(function () {
+    //     $(this).css("box-shadow","none");
+    // });
+    var textInput = $("input[type='text']"); 
 
-    var arrow_Right = 39;
-
-    $("html").keydown(function(event){
-        if(event.which == arrow_Right){
-            $(".blue-box").stop().animate({
-                marginLeft: "+=10px"
-            },50);
-            //$(".blue-box").css("margin-left","+=10px");
+    function CheckTextValidation(){
+        var textInput = $("input[type='text']");    
+        var text = textInput.val();
+        if (text.length < 3) {
+            textInput.css("box-shadow", "0 0 12px #C4200D");
         }
+        else {
+            textInput.css("box-shadow", "0 0 12px #139901");
+        }
+    }
+    
+    // textInput.focus(function () {
+    //     CheckTextValidation();
+    // });
+    // textInput.blur(function () {
+    //     CheckTextValidation();
+    // });
+
+    textInput.on("focus blur keydown",function(){
+        CheckTextValidation();
     });
+
 });
 
 
