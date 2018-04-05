c1ca7bc93eb86acca02c15910e3587896ea4c180
unknown <deepak.b@Deepak.sgp.in>
2018-04-05 19:52:39 +0530

    Submit Event

--- js/script.js
+++ js/script.js
@@ -1,18 +1,10 @@
 $(document).ready(function () {
-    // $("#formcheckbox").change(function() {
-    //     var isChecked = $(this).prop("checked");
-    //     if(isChecked){
-    //         $(this).add("label[for='cb']").css("box-shadow","0 0 4px #181");
-    //     }
-    //     else{
-    //         $(this).add("label[for='cb']").css("box-shadow","0 0 4px #811");
-    //     }
-    // });
-
-    $("#selection").change(function() {
-        //var selectedOption = $("#selection option:selected").text();
-        var selectedOption = $(this).find(":selected").text();
-        alert(selectedOption);
+    $("#form").submit(function(event) {
+        var textAreadata = $("#textarea").val().trim();
+        if(textAreadata==""){
+            $("#textarea").css("box-shadow","0 0 4px #811");
+            event.preventDefault();
+        }
     });
 });
 
