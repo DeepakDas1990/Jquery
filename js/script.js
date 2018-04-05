c079cd1dbb8b9f6cede87adf7637406af1fa8781
unknown <deepak.b@Deepak.sgp.in>
2018-04-05 19:37:44 +0530

    Using Change Event

--- js/script.js
+++ js/script.js
@@ -1,35 +1,19 @@
 $(document).ready(function () {
-    // var inputFields = $("input[type='text'], input[type='email'], textarea");
-    // inputFields.focus(function(){
-    //     $(this).css("box-shadow","0 0 8px #666");
-    // });
-    // inputFields.blur(function () {
-    //     $(this).css("box-shadow","none");
-    // });
-    var textInput = $("input[type='text']"); 
-
-    function CheckTextValidation(){
-        var textInput = $("input[type='text']");    
-        var text = textInput.val();
-        if (text.length < 3) {
-            textInput.css("box-shadow", "0 0 12px #C4200D");
-        }
-        else {
-            textInput.css("box-shadow", "0 0 12px #139901");
-        }
-    }
-    
-    // textInput.focus(function () {
-    //     CheckTextValidation();
-    // });
-    // textInput.blur(function () {
-    //     CheckTextValidation();
+    // $("#formcheckbox").change(function() {
+    //     var isChecked = $(this).prop("checked");
+    //     if(isChecked){
+    //         $(this).add("label[for='cb']").css("box-shadow","0 0 4px #181");
+    //     }
+    //     else{
+    //         $(this).add("label[for='cb']").css("box-shadow","0 0 4px #811");
+    //     }
     // });
 
-    textInput.on("focus blur keydown",function(){
-        CheckTextValidation();
+    $("#selection").change(function() {
+        //var selectedOption = $("#selection option:selected").text();
+        var selectedOption = $(this).find(":selected").text();
+        alert(selectedOption);
     });
-
 });
 
 
